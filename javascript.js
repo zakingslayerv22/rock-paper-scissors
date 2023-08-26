7eedd386ff1d66d17dfb8df06514da75f5487767
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-08-26 11:21:21 +0200

    Write the playRound(playerSelection getComputerChoice) function
    The function plays a single round of the game
    between the computer and player. It first creates
    a sample variable playerSelection = 'rock' and
    returns the result depending on the playerSelection
    and computerChoice and logs it to the console.

--- javascript.js
+++ javascript.js
@@ -9,5 +9,38 @@ function getComputerChoice() {
     }
 
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
+function playRound(playerSelection, getComputerChoice) {
+    if (playerSelection == getComputerChoice) {
+        return 'Draw!';}
+    else if (playerSelection == 'rock'){
+            if (getComputerChoice == 'paper'){
+                return "You Lose! Paper beats Rock";
+            }else return "You Win! Rock Beats Scissors";
+        }
+    else if (playerSelection == 'paper'){
+            if (getComputerChoice == 'rock'){
+                return "You Win! Paper beats Rock";
+            }else return "You Lose! Scissors beats Paper";
+    }else if (playerSelection == 'scissors'){
+            if (getComputerChoice == 'rock'){
+                return "You Lose! Rock beats Scissors";
+            }else return "You Win! Scissors beats Paper";
+    }
+    
+}
+
+const playerSelection = 'rock';
+const computerChoice = getComputerChoice();
+console.log(playRound(playerSelection, computerChoice));
+
+// var words = ['rock',
+// 'paper',
+// 'scissors'
+// ];
+// function randomWord(arr) {
+//     return arr[Math.floor(Math.random() * arr.length)];
+// }
+
+// alert(randomWord(words));
