bc2b33b14be0a10a9abae599e3089c90d4126896
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-08-26 12:19:11 +0200

    Write the game() function.
    The game() plays the game 5 times.
    It creates new variables for playerSelection
    and getComputerChoice for each round.
    
    It could be done better with the for loop. Loop
    5 times.

--- javascript.js
+++ javascript.js
@@ -9,6 +9,9 @@ function getComputerChoice() {
     }
 
 }
+
+getComputerChoice();
+
 // console.log(getComputerChoice());
 
 function playRound(playerSelection, getComputerChoice) {
@@ -31,9 +34,35 @@ function playRound(playerSelection, getComputerChoice) {
     
 }
 
-const playerSelection = 'rock';
-const computerChoice = getComputerChoice();
-console.log(playRound(playerSelection, computerChoice));
+// const playerSelection = 'rock';
+// const computerChoice = getComputerChoice();
+// console.log(playRound(playerSelection, computerChoice));
+
+function game() {
+
+    const playerSelection1 = prompt('Rock, Paper, Scissors?');
+    const computerSelection1 = getComputerChoice();
+    console.log(playRound(playerSelection1, computerSelection1));
+
+    const playerSelection2 = prompt('Rock, Paper, Scissors?');
+    const computerSelection2 = getComputerChoice();
+    console.log(playRound(playerSelection2, computerSelection2));
+
+    const playerSelection3 = prompt('Rock, Paper, Scissors?');
+    const computerSelection3 = getComputerChoice();
+    console.log(playRound(playerSelection3, computerSelection3));
+
+    const playerSelection4 = prompt('Rock, Paper, Scissors?');
+    const computerSelection4 = getComputerChoice();
+    console.log(playRound(playerSelection4, computerSelection4));
+
+    const playerSelection5 = prompt('Rock, Paper, Scissors?');
+    const computerSelection5 = getComputerChoice();
+    console.log(playRound(playerSelection5, computerSelection5));
+
+}
+
+game();
 
 // var words = ['rock',
 // 'paper',
