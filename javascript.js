71f22f620c9ed729086312f67fe6d09c1ec05695
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-10-13 13:38:40 +0200

    Add div to display scores and refactor the playRound() function
    Add the player-score, tie-score and computer-score divs
    in the index.html. Then refactored the playRound()
    function to display generic results if player or
    computer wins (or a tie) so I can handle score display
    with this results. Manipulate the DOM to display the
    scores and return dynamic message.

--- javascript.js
+++ javascript.js
@@ -1,59 +1,106 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
-        return 'rock';
+        return 'Rock';
     } else if (randomNumber === 2) {
-        return 'paper';
+        return 'Paper';
     } else {
-        return 'scissors';
+        return 'Scissors';
     }
 
 }
 
-getComputerChoice();
+// getComputerChoice();
 
-// console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
 
 function playRound(playerSelection, getComputerChoice) {
-    if (playerSelection == getComputerChoice) {
-        return 'Draw!';}
-    else if (playerSelection == 'rock'){
-            if (getComputerChoice == 'paper'){
-                return "You Lose! Paper beats Rock";
-            }else return "You Win! Rock Beats Scissors";
-        }
-    else if (playerSelection == 'paper'){
-            if (getComputerChoice == 'rock'){
-                return "You Win! Paper beats Rock";
-            }else return "You Lose! Scissors beats Paper";
-    }else if (playerSelection == 'scissors'){
-            if (getComputerChoice == 'rock'){
-                return "You Lose! Rock beats Scissors";
-            }else return "You Win! Scissors beats Paper";
-    }
+    let message = "Computer picked " + getComputerChoice + "\n";
+    let result = '';
     
+    console.log(message);
+    if (playerSelection == getComputerChoice) {
+        result = "draw";
+        //return message += "It's a draw."; it wont capture the value
+        //of the result variable because there's a return statement
+        // Since the return statement will exit the if condition,
+        // the content of the result variable will not be able 
+        //leave (or be captured)
+    }
+    else if (playerSelection == 'Rock'){
+            if (getComputerChoice == 'Paper'){
+                result = "you lose";//paper beats rock
+                
+            }else {
+                result = "you win";//rock beats scissors
+                
+            }
+        }
+    else if (playerSelection == 'Paper'){
+            if (getComputerChoice == 'Rock'){
+                
+                result = "you win";//paper beats rock
+               
+            }else {
+                result = "you lose";//scissors beats paper
+                
+            }
+    }else if (playerSelection == 'Scissors'){
+            if (getComputerChoice == 'Rock'){
+                result = "you lose";//rock beats scissors
+                
+            }else {
+                result = "you win";//scissors beats paper
+                
+            }
+
+            
+    }
+    const playerScoreDisplay = document.querySelector('#player-score');
+    const computerScoreDisplay = document.querySelector('#computer-score');
+    const tieScoreDisplay = document.querySelector('#tie-score');
+    
+
+    if (result === 'draw'){
+        tieScore++;
+        tieScoreDisplay.textContent = tieScore;
+        return "It's a draw! " + playerSelection + " and " + getComputerChoice + ".";
+    }
+    else if (result === 'you win'){
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        return "You Win! " + playerSelection + " beats " + getComputerChoice +".";
+    }else if (result === 'you lose'){
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        return "You Lose! " + getComputerChoice + " beats " + playerSelection+".";
+    }
+
 }
 
 
 const buttonsDiv = document.querySelector('#buttons');
 buttonsDiv.addEventListener('click', (event) => {
-
-  //the output paragraph
-  let output = document.querySelector('#output');
+  
+  let output = document.querySelector('#output');//the output paragraph
 
   let buttonClicked = event.target;
 
   switch (buttonClicked.id) {
     case 'rock':
-      output.textContent = playRound('rock', getComputerChoice());
+      output.textContent = playRound('Rock', getComputerChoice());
       break;
 
     case 'paper':
-      output.textContent = playRound('paper', getComputerChoice());
+      output.textContent = playRound('Paper', getComputerChoice());
       break;
 
     case 'scissors':
-      output.textContent = playRound('scissors', getComputerChoice());
+      output.textContent = playRound('Scissors', getComputerChoice());
       break;
   }
 
