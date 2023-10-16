1df27c2034edf98242345f1ba5881a12b8b9b380
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-10-16 18:43:16 +0200

    Disable buttons appropriately and create resetGame() function
    Add a html start button and enabled only it on window
    load and on reset while disabling others. Disabled the
    start button during gameplay and enabled others.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,29 @@
+// define buttons 
+
+//i used a switch statement later down because i want to too much
+//event listeners will directly impact page performance
+//see Event delegation
+//https://www.javascripttutorial.net/javascript-dom/javascript-event-delegation/
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+let startButton = document.querySelector('#start-and-reset');
+
+
+//start the game 
+const enableGameButtons = () => {
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+    startButton.disabled = true;
+}
+
+startButton.addEventListener('click', () => {
+enableGameButtons();
+})
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
@@ -17,12 +43,77 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 let tieScore = 0;
+let gameWinner = '';
+   
+//the score display divs
+const playerScoreDisplay = document.querySelector('#player-score');
+const computerScoreDisplay = document.querySelector('#computer-score');
+const tieScoreDisplay = document.querySelector('#tie-score');
+
+
+//disable game buttons except start after the resetGame()
+const disableGameButtons = () => {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+    startButton.disabled = false;
+};
+
+window.addEventListener('load', (event) => {
+    disableGameButtons();
+});
+
+
+//reset the game
+const resetGame = (whoWon) => {
+    let prompt = "Game Over! \n";
+    if (whoWon === "user") {
+      prompt += "You won this round with a score of 5!\n";
+      playerScoreDisplay.textContent = "5";
+    } else {
+      prompt += "You lose! Computer won the game with a score of 5.\n";
+       computerScoreDisplay.textContent = "5";
+    }
+    confirm(`${prompt} Do you want to restart?`);
+    playerScore = 0;
+    computerScore = 0;
+    tieScore = 0;
+    playerScoreDisplay.textContent = "";
+    computerScoreDisplay.textContent = "";
+    tieScoreDisplay.textContent = "";
+    document.querySelector("#output").textContent = "New Game Started! Yeah you're the man!"
+   
+    disableGameButtons();
+
+    
+  };
+
+  
+
 
 function playRound(playerSelection, getComputerChoice) {
     let message = "Computer picked " + getComputerChoice + "\n";
     let result = '';
     
     console.log(message);
+
+    if (playerScore === 5){
+        console.log(gameWinner = "You Win this round!");
+    }else if (computerScore === 5){
+        console.log(gameWinner = "You Lose! Computer wins this round!");
+    }
+
+    if(playerScore === 5 || computerScore === 5){
+        document.querySelector('#rock').disabled = true;
+        document.querySelector('#paper').disabled = true;
+        document.querySelector('#scissors').disabled = true;
+        // document.querySelector('start-and-reset')
+    }
+
+
+    
+
+    //determine the winner of a section
     if (playerSelection == getComputerChoice) {
         result = "draw";
         //return message += "It's a draw."; it wont capture the value
@@ -31,17 +122,17 @@ function playRound(playerSelection, getComputerChoice) {
         // the content of the result variable will not be able 
         //leave (or be captured)
     }
-    else if (playerSelection == 'Rock'){
+    else if (playerSelection === 'Rock'){
             if (getComputerChoice == 'Paper'){
                 result = "you lose";//paper beats rock
-                
+            
             }else {
                 result = "you win";//rock beats scissors
                 
             }
         }
-    else if (playerSelection == 'Paper'){
-            if (getComputerChoice == 'Rock'){
+    else if (playerSelection === 'Paper'){
+            if (getComputerChoice === 'Rock'){
                 
                 result = "you win";//paper beats rock
                
@@ -49,22 +140,19 @@ function playRound(playerSelection, getComputerChoice) {
                 result = "you lose";//scissors beats paper
                 
             }
-    }else if (playerSelection == 'Scissors'){
-            if (getComputerChoice == 'Rock'){
+    }else if (playerSelection === 'Scissors'){
+            if (getComputerChoice === 'Rock'){
                 result = "you lose";//rock beats scissors
                 
             }else {
                 result = "you win";//scissors beats paper
                 
-            }
-
-            
+            }    
     }
-    const playerScoreDisplay = document.querySelector('#player-score');
-    const computerScoreDisplay = document.querySelector('#computer-score');
-    const tieScoreDisplay = document.querySelector('#tie-score');
-    
 
+
+
+    //display scores
     if (result === 'draw'){
         tieScore++;
         tieScoreDisplay.textContent = tieScore;
@@ -73,14 +161,22 @@ function playRound(playerSelection, getComputerChoice) {
     else if (result === 'you win'){
         playerScore++;
         playerScoreDisplay.textContent = playerScore;
+        if(playerScore === 5){
+            resetGame("user");
+          }
         return "You Win! " + playerSelection + " beats " + getComputerChoice +".";
     }else if (result === 'you lose'){
         computerScore++;
         computerScoreDisplay.textContent = computerScore;
+        if(computerScore === 5){
+            resetGame("computer");
+          }
         return "You Lose! " + getComputerChoice + " beats " + playerSelection+".";
     }
 
+
 }
+
 
 
 const buttonsDiv = document.querySelector('#buttons');
